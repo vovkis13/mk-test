372a976df072bd4c01bcf4a5fc701a4e23981415
Volodymyr Klochan <vovkis13@gmail.com>
2022-06-22 23:24:04 +0300

    fix public path

--- src/components/Sound/Sound.tsx
+++ src/components/Sound/Sound.tsx
@@ -7,7 +7,7 @@ interface SoundProps {
 export const Sound = ({ id }: SoundProps): JSX.Element => {
   return (
     <audio id={id}>
-      <source src={`/audio/${id}.mp3`} type="audio/mp3"></source>
+      <source src={`${process.env.PUBLIC_URL}/audio/${id}.mp3`} type="audio/mp3"></source>
     </audio>
   );
 };
